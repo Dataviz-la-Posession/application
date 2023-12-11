251c2c406e2dd59f15d1441a9c9f76462c6d6f01
gabs974 <gael.lisador@tco.re>
2023-12-11 08:10:07 +0400

    ajout da dataviz et volet fitre à termimer

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 from api import population
 import plotly.express as px
+import random  # Ajout de l'importation du module random
+#import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 
 # Configurer la page pour utiliser une mise en page en pleine largeur
@@ -14,16 +18,26 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center'>Dataviz</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center'>L'évolution de la population dans une collectivité territoriale</h1>", unsafe_allow_html=True)
 
 # Obtenez les années uniques dans les données
 annees = population['annee_utilisation'].unique()
 annees.sort()  # Assurez-vous que les années sont dans l'ordre
 
+
+
+
 # Créer un curseur pour sélectionner l'année
 annee_selectionnee = st.slider("Sélectionnez l'année", int(annees.min()), int(annees.max()), int(annees.max()))
 annee_selectionnee_str = str(annee_selectionnee)
 population_filtree = population[population['annee_utilisation'] == annee_selectionnee_str]
+
+#indicateur kpi population total
+# Calculer votre indicateur KPI (par exemple, la somme de la population)
+kpi_value = population_filtree['population_totale'].sum()
+
+# Afficher l'indicateur KPI
+st.markdown(f"### Population Totale : {kpi_value}")
 
 # Création de la treemap
 treemap = px.treemap(population_filtree,
@@ -51,12 +65,85 @@ for trace in sunburst.data:
 
 
 # Utilisation des onglets avec des colonnes pour centrer le contenu
-tab1, tab2 = st.tabs(["Répartition de la Population", "Détail par Commune"])
+#tab1, tab2 = st.tabs(["Répartition de la Population", "Détail par Commune"])
 
-with tab1:
+
+#with tab1:
+#st.plotly_chart(treemap, use_container_width=True)
+
+#with tab2:
+#st.plotly_chart(sunburst, use_container_width=True)
+col1, col2 = st.columns(2)
+
+# Afficher le treemap dans la première colonne
+with col1:
     st.plotly_chart(treemap, use_container_width=True)
 
-with tab2:
+# Afficher le sunburst dans la deuxième colonne
+with col2:
     st.plotly_chart(sunburst, use_container_width=True)
 
+
+# Diviser l'espace en deux colonnes pour la nouvelle ligne
+#col3, col4 = st.columns(2)
+
+# Ajouter du contenu dans la première colonne de la nouvelle ligne
+#with col3:
+#    st.write("Contenu de la colonne 1 de la nouvelle ligne")
+
+# Ajouter du contenu dans la deuxième colonne de la nouvelle ligne
+#with col4:
+#    st.write("Contenu de la colonne 2 de la nouvelle ligne")
+
+
+
+
+# Exemple de données
+annees = pd.DataFrame({'annee_utilisation': pd.date_range(start='2009', end='2019', freq='Y')})
+
+# Ajouter un volet de filtre dans une barre latérale distincte
+sidebar = st.sidebar
+annee_selectionnee = sidebar.slider("Sélectionnez l'année", int(annees['annee_utilisation'].dt.year.min()), int(annees['annee_utilisation'].dt.year.max()), int(annees['annee_utilisation'].dt.year.max()))
+
+# Afficher seulement le filtre
+#st.write(f"Année sélectionnée : {annee_selectionnee}")
+
+# Afficher la population filtrée (ou d'autres contenus si nécessaire)
+#population_filtree = annees[annees['annee_utilisation'].dt.year == annee_selectionnee]
+#st.write(population_filtree)
+
+
+
+annee_selectionner = sidebar.selectbox("Sélectionnez l'année", annees['annee_utilisation'].dt.year.unique())
+
+
+
+
+# Afficher deux graphiques pour les indicateurs barre horyzonta
+#st.subheader("Année N")
+#st.metric("Population", kpi_value)
+
+#st.subheader("Année N-1")
+#st.metric("Population", kpi_value * 2)
+
+# Créer un graphique avec Plotly Express
+fig = px.bar(population_filtree, x='nom_de_la_commune', y=['population_totale', 'population_totale'],
+             labels={'value': 'Population', 'variable': 'Indicateur'},
+             title=f'Indicateurs superposés pour {annee_selectionnee}')
+
+# Afficher le graphique avec Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+###############################################graphique en barre horyzontal
+
+fig1 = px.bar(population_filtree, x='population_totale', y='nom_de_la_commune', orientation='h',
+             labels={'population_totale': 'Population Totale', 'nom_de_la_commune': 'Nom de la Commune'},
+             title=f"Évolution de la population en {annee_selectionnee}")
+
+# Inverser l'ordre des barres pour avoir la plus récente en haut
+fig1.update_yaxes(categoryorder='total ascending')
+
+# Afficher le graphique avec Streamlit
+st.plotly_chart(fig1, use_container_width=True)
 
